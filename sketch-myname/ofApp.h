a3194a22471c0f76315c4f947daf212fdca1a124
dchua28 <166455093+dchua28@users.noreply.github.com>
2024-04-08 18:13:23 -0500

    Update ofApp.h
    Put in code for ofApp class.

--- sketch-myname/ofApp.h
+++ sketch-myname/ofApp.h
@@ -1,1 +1,40 @@
 
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	static const int WindowWidth = 1300;
+	static const int WindowHeight = 768;
+
+	const int TextXCoord = 90;
+	const int TextYCoord = 90;
+	const int TextWidth = 1050 + 135 - 90;
+	const int TextHeight = 250 + 135 - 90;
+
+	bool checkTextBounds(const int x, const int y);
+};
+
+struct backgroundColor {
+	int red{};
+	int green{};
+	int blue{};
+};
